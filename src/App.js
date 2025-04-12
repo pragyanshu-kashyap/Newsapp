bedd3f3c2c326408278aa4ae6667e90aa256cb6f
pragyanshu-kashyap <your-github-pragyanshukashyap30092001.com>
2025-04-12 19:45:13 +0530

    modified some files

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ export default class App extends Component {
         <Router>
           <Navbar/>
           <Routes>
+
               <Route exact path="/" element={<News key={"general"} pageSize={10} country={'us'} category={'general'} />} />
               <Route exact path="/business" element={<News key={"business"} pageSize={10} country={'us'} category={'business'} />} />
               <Route exact path="/entertainment" element={<News key={"entertainment"} pageSize={10} country={'us'} category={'entertainment'} />} />
@@ -24,6 +25,9 @@ export default class App extends Component {
               <Route exact path="/science" element={<News key={"science"} pageSize={10} country={'us'} category={'science'} />} />
               <Route exact path="/sports" element={<News key={"sports"} pageSize={10} country={'us'} category={'sports'} />} />
               <Route exact path="/technology" element={<News key={"technology"} pageSize={10} country={'us'} category={'technology'} />} />
+
+
+              {/* <Route exact path="/Listings" element={<Listings key={"listings"}></Listings> }/> */}
           </Routes>
         </Router>
       </div>
